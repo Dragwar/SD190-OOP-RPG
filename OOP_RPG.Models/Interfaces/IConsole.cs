f16ab91a5dc3f46e6918be41a516a2e47cd6fb38
Everett Grassler <everettag@mymts.net>
2019-09-24 15:56:44 -0700

    Added IConsole and a implementation

---
+++ OOP_RPG.Models/Interfaces/IConsole.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+
+namespace OOP_RPG.Models.Interfaces
+{
+    public interface IConsole
+    {
+        StringBuilder Title { get; }
+        int CursorLeft { get; set; }
+        int CursorSize { get; set; }
+        int CursorTop { get; set; }
+        bool IsCursorVisible { get; set; }
+        bool KeyAvailable { get; }
+        bool CapsLock { get; }
+        bool NumLock { get; }
+        ConsoleColor BackgroundColor { get; set; }
+        ConsoleColor TextColor { get; set; }
+        void ResetColor();
+        int Read();
+        ConsoleKeyInfo ReadKey();
+        ConsoleKeyInfo ReadKey(bool intercept);
+        string ReadLine();
+        void Clear();
+        void SetCursorPosition(int left, int top);
+        void Write(bool value);
+        void Write(char value);
+        void Write(char[] buffer);
+        void Write(decimal value);
+        void Write(double value);
+        void Write(float value);
+        void Write(int value);
+        void Write(long value);
+        void Write(object value);
+        void Write(string value);
+        void Write(string format, params object[] args);
+        void Write(uint value);
+        void Write(ulong value);
+        void WriteLine();
+        void WriteLine(bool value);
+        void WriteLine(char value);
+        void WriteLine(char[] buffer);
+        void WriteLine(decimal value);
+        void WriteLine(double value);
+        void WriteLine(float value);
+        void WriteLine(int value);
+        void WriteLine(long value);
+        void WriteLine(object value);
+        void WriteLine(string value);
+        void WriteLine(string format, params object[] args);
+        void WriteLine(uint value);
+        void WriteLine(ulong value);
+    }
+}
---
+++ OOP_RPG.Models/Terminal.cs
@@ -0,0 +1,61 @@
+﻿using OOP_RPG.Models.Interfaces;
+using System;
+using System.Text;
+
+namespace OOP_RPG.Models
+{
+    public class Terminal : IConsole
+    {
+        public ConsoleColor TextColor { get; set; }
+        public ConsoleColor BackgroundColor { get; set; }
+        public StringBuilder Title { get; }
+        public bool IsCursorVisible { get; set; }
+        public bool CapsLock { get; }
+        public bool NumLock { get; }
+        public bool KeyAvailable { get; }
+        public int CursorTop { get; set; }
+        public int CursorLeft { get; set; }
+        public int CursorSize { get; set; }
+
+        public void SetCursorPosition(int left, int top) => Console.SetCursorPosition(left, top);
+
+        public void Clear() => Console.Clear();
+        public void ResetColor() => Console.ResetColor();
+
+        public int Read() => Console.Read();
+        public string ReadLine() => Console.ReadLine();
+        public ConsoleKeyInfo ReadKey() => Console.ReadKey();
+        public ConsoleKeyInfo ReadKey(bool intercept) => Console.ReadKey(intercept);
+
+
+        public void Write(bool value) => Console.Write(value);
+        public void Write(decimal value) => Console.Write(value);
+        public void Write(double value) => Console.Write(value);
+        public void Write(float value) => Console.Write(value);
+        public void Write(int value) => Console.Write(value);
+        public void Write(uint value) => Console.Write(value);
+        public void Write(long value) => Console.Write(value);
+        public void Write(ulong value) => Console.Write(value);
+        public void Write(object value) => Console.Write(value);
+        public void Write(char value) => Console.Write(value);
+        public void Write(char[] buffer) => Console.Write(buffer);
+        public void Write(string value) => Console.Write(value);
+        public void Write(string format, params object[] args) => Console.Write(format, args);
+
+
+        public void WriteLine() => Console.WriteLine();
+        public void WriteLine(bool value) => Console.WriteLine(value);
+        public void WriteLine(decimal value) => Console.WriteLine(value);
+        public void WriteLine(double value) => Console.WriteLine(value);
+        public void WriteLine(float value) => Console.WriteLine(value);
+        public void WriteLine(int value) => Console.WriteLine(value);
+        public void WriteLine(uint value) => Console.WriteLine(value);
+        public void WriteLine(long value) => Console.WriteLine(value);
+        public void WriteLine(ulong value) => Console.WriteLine(value);
+        public void WriteLine(object value) => Console.WriteLine(value);
+        public void WriteLine(char value) => Console.WriteLine(value);
+        public void WriteLine(char[] buffer) => Console.WriteLine(buffer);
+        public void WriteLine(string value) => Console.WriteLine(value);
+        public void WriteLine(string format, params object[] args) => Console.WriteLine(format, args);
+    }
+}
